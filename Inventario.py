0e59dfaba743531bd8f602ff88a70f185a9a58f5
Valeria Cafarelli <valgamer21c2@gmail.com>
2026-06-08 18:59:00 -0400

    Metodos de buscar y agregar prodcutos

--- Inventario.py
+++ Inventario.py
@@ -2,22 +2,29 @@ from Producto import *
 from Estadistica_producto import *
 class Inventario:
     def __init__(self):
+        self.columnas =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         self.productos = []
         self.estadistica_productos = []
     def buscar_producto(self, coordenadas):
         for fila in range(len( self.prductos)):
-            if fila == coordenadas[1]:
+            if fila +1 == coordenadas[1]:
                 for producto in range(len(fila)):
-                    if producto == coordenadas[0]:
+                    if self.columnas[producto] == coordenadas[0]:
                         return fila[producto]
-    def agregar_prducto(self):
+    def agregar_prducto(self, coordenada):
         nombre=input("Ingrese nombre del producto")
         precio=input("Ingrese el precio del producto")
         cantidad=input("Ingrese la cantidad")
         producto=Producto(nombre, precio)
-        self.productos.append(producto)
         estadistica=Estadistica_producto(producto, cantidad)
-        self.estadistica_productos.append(estadistica)
+        for fila in range(len(self.productos)):
+            if fila +1 == coordenada [1]:
+                for p in range(len(fila)):
+                    if self.columnas[p]== coordenada[0]:
+                        self.productos.insert(p, producto)
+                        self.estadistica_productos.insert(p, estadistica)
+
+        
                         
 
                     
