28a3f014653d2b6887bfeb6ae62e3d108666e4c4
Valeria Cafarelli <valgamer21c2@gmail.com>
2026-06-08 18:18:22 -0400

    Metodo de agregar productos

--- Inventario.py
+++ Inventario.py
@@ -1,3 +1,5 @@
+from Producto import *
+from Estadistica_producto import *
 class Inventario:
     def __init__(self):
         self.productos = []
@@ -8,7 +10,14 @@ class Inventario:
                 for producto in range(len(fila)):
                     if producto == coordenadas[0]:
                         return fila[producto]
-                    
+    def agregar_prducto(self):
+        nombre=input("Ingrese nombre del producto")
+        precio=input("Ingrese el precio del producto")
+        cantidad=input("Ingrese la cantidad")
+        producto=Producto(nombre, precio)
+        self.productos.append(producto)
+        estadistica=Estadistica_producto(producto, cantidad)
+        self.estadistica_productos.append(estadistica)
                         
 
                     
